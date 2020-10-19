255d6de58fdf34edc94ea94fa3c0e8bef2969501
Alexey Smyshlaev <alexeysm@playtika.com>
2020-10-19 18:19:16 +0300

    CustomInspector

---
+++ Assets/Editor/WallEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Wall))]
+public class WallEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        var script = (Wall)target;
+        if (GUILayout.Button("DefaultColor"))
+            script.SetDefaultColor();
+    }
+}
---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class Wall : MonoBehaviour
+{
+    public void SetDefaultColor()
+    {
+        var meshRenderer = GetComponent<MeshRenderer>();
+        var material = meshRenderer.sharedMaterial;
+        material.color = Color.white;
+        meshRenderer.sharedMaterial = material;
+    }
+}
