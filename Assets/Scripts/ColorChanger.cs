6ee18ace2f684d9731c691a7905536255e0c5386
Alexey Smyshlaev <alexeysm@playtika.com>
2020-10-19 17:55:55 +0300

    MenuItem

---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ColorChanger : MonoBehaviour
+{
+    [MenuItem("GameObject/ChangeColors")]
+    public static void ChangeColors()
+    {
+        foreach (var gameObject in FindObjectsOfType<MeshRenderer>())
+        {
+            var material = gameObject.sharedMaterial;
+            material.color = new Color(
+                Random.Range(0f, 1f),
+                Random.Range(0f, 1f),
+                Random.Range(0f, 1f));
+            gameObject.sharedMaterial = material;
+        }
+    }
+}
